cc72471e2070997440320437fc70c46e4b2f72ea
master-senses <hrishikeshkalyanaraman@gmail.com>
2025-06-22 19:08:17 -0500

    changed tool name

--- app/MoodboardShape/ui-overrides.tsx
+++ app/MoodboardShape/ui-overrides.tsx
@@ -1,19 +1,14 @@
-import { TLUiOverrides } from 'tldraw'
-import type { Editor, TLUiToolsContextType } from 'tldraw'
+import { TLUiOverrides } from "tldraw"
 
 export const uiOverrides: TLUiOverrides = {
-  tools(editor: Editor, tools: TLUiToolsContextType) {
-    // Add your custom tool to the toolbar
-    tools['moodboard-shape'] = {
-      id: 'moodboard-shape',
-      icon: 'image', // You can use any built-in icon or create custom ones
-      label: 'Moodboard',
-      readonlyOk: false,
-      onSelect: () => {
-        editor.setCurrentTool('moodboard-shape')
-      },
-    }
-    return tools
-  },
-  // Note: The toolbar order can be customized via the Tldraw UI components in later versions.
+	tools(_editor, tools) {
+		return {
+			...tools,
+			frame: {
+				...tools.frame,
+				label: 'Moodboard',
+        id: 'moodboard',
+			},
+		}
+	},
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,7 @@
 import dynamic from 'next/dynamic'
 import 'tldraw/tldraw.css'
 import { MakeRealButton } from './components/MakeRealButton'
-import { RiskyButCoolAPIKeyInput } from './components/RiskyButCoolAPIKeyInput'
 import { PreviewShapeUtil } from './PreviewShape/PreviewShape'
-import {MoodboardTool} from './MoodboardShape/MoodboardShape'
 import { uiOverrides } from './MoodboardShape/ui-overrides'
 
 const Tldraw = dynamic(async () => (await import('tldraw')).Tldraw, {
@@ -17,7 +15,6 @@ const components = {
 }
 
 const shapeUtils = [PreviewShapeUtil]
-const tools = [MoodboardTool]
 
 export default function App() {
 	return (
@@ -26,11 +23,12 @@ export default function App() {
 				persistenceKey="make-real"
 				components={components}
 				shapeUtils={shapeUtils}
-				tools={tools}
 				overrides={uiOverrides}
 			>
 				{/* <RiskyButCoolAPIKeyInput /> */}
 			</Tldraw>
 		</div>
-	)
+	) 
 }
+
+	
